5dbe1214fdf89f522e264536082fc411e96bc635
Wayne Logan <loganamollo@gmail.com>
2024-08-30 17:58:42 +0300

    adds pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
-import { Box, Heading, HStack, Text, VStack } from "@chakra-ui/react"
+import { Box, Button, Heading, HStack, Text, VStack } from "@chakra-ui/react"
 import { Post, posts } from "./data/blogData"
 import { useEffect, useState } from "react"
 import PostCard from "@/components/PostCard"
 
 export default function Home() {
   const [blogPosts, setBlogPosts] = useState<Post[]>([])
+  const [numPages, setNumPages] = useState(0)
+  const pageSize = 6
+  const [currentPage, setCurrentPage] = useState(0)
 
   const getPosts = (): Post[] => {
     return posts
@@ -14,7 +17,13 @@ export default function Home() {
   useEffect(() => {
     const fetchedPosts = getPosts()
     setBlogPosts(fetchedPosts)
+    const pages = Math.floor(fetchedPosts.length / pageSize)
+    setNumPages(pages)
   }, [])
+
+  const startIndex = pageSize * currentPage
+  const lastIndex = startIndex + pageSize
+  const currentPosts = blogPosts.slice(startIndex, lastIndex)
 
 
   return (
@@ -41,11 +50,46 @@ export default function Home() {
           gap={20}
           flexWrap={'wrap'}
         >
-          {blogPosts.map((post) => {
+          {currentPosts.map((post) => {
             const index = blogPosts.indexOf(post)
             const slug = post.title.split(' ').join('-')
             return <PostCard key={index} slug={slug} {...post} />
           })}
+        </HStack>
+        <HStack
+          my={10}
+        >
+          <Button
+            fontSize={32}
+            onClick={() => setCurrentPage((formerCurrentPage) => (
+              0 < formerCurrentPage ?  
+              formerCurrentPage - 1 : currentPage
+            ))}
+          >
+            &lsaquo;
+          </Button>
+          {
+            // generates range by flattening an iterator for an empty array of desired length
+            Array.from(Array(numPages).keys()).map(index => (
+              <Button
+                key={Number(index)}
+                bgColor={currentPage == index ? '#012B55' : ''}
+                color={currentPage == index ? 'white' : ''}
+                onClick={() => setCurrentPage(index)}
+              >
+                {index + 1}
+              </Button>
+            ))
+          }
+          <Button
+            fontSize={32}
+            onClick={() => setCurrentPage((formerCurrentPage) => (
+              formerCurrentPage < numPages - 1 ?  
+              formerCurrentPage + 1 : currentPage
+            ))}
+          >
+            &rsaquo;
+          </Button>
         </HStack>
       </VStack>
     </main>
